d4b33ac03167c1fbb972f6b0f586772c37641c41
Piotr1215 <decoder@live.de>
2017-02-21 21:39:33 +0100

    Core specs

---
+++ CoreWithBehave.Specs/TestFeature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xbehave;
+
+using Xunit;
+
+namespace CoreWithBehave.Specs
+{
+    public class TestFeature
+    {
+        [Scenario]
+        public void ScenarioName(int one, int two, int res)
+        {
+            "Given AAA"
+                .x(() =>
+                {
+                    one = 1;
+                    two = 2;
+                });
+
+            "When BBB"
+                .x(() => res = one + two);
+
+            "Then CCC"
+                .x(() => Assert.Equal(res, 3));
+        }
+    }
+}
