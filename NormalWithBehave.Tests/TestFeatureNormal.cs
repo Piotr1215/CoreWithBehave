df0bee18c9ff72dec071eecf70e7c3f5da18f9ca
Piotr1215 <decoder@live.de>
2017-02-21 21:30:18 +0100

    Add projects

---
+++ NormalWithBehave.Tests/TestFeatureNormal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xbehave;
+
+using Xunit;
+
+namespace NormalWithBehave.Tests
+{
+    public class TestFeatureNormal
+    {
+        [Scenario]
+        public void ScenarioName(int one, int two, int res)
+        {
+            "Given AAA"
+                .x(() =>
+                {
+                    one = 1;
+                    two = 2;
+                });
+
+            "When BBB"
+                .x(() => res = one + two);
+
+            "Then CCC"
+                .x(() => Assert.Equal(res, 3));
+        }
+    }
+}
